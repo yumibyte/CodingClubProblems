26f31a43b44b44201c8b381ac031c2b01774f750
Yumibyte <ashraigosa@gmail.com>
2020-10-15 16:26:06 -0700

    added problems for october 15

---
+++ AdvancedProblems/src/TeacherAverage.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TeacherAverage {
+
+    public static void main(String args[]) {
+        ArrayList<Double> grades = new ArrayList<Double>();
+        double average = 0.0;
+        double sum = 0.0;
+        int belowAverage = 0;
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+//
+//        int x = 0;
+//        while (scanner.hasNext()) {
+//            System.out.println("Num: ");
+//            grades.add(scanner.nextDouble());
+//            sum = sum + grades.get(x);
+//            //sum = sum + currentInput;
+//            x += 1;
+//        }
+        System.out.println(sum);
+
+
+//        average = sum / grades.length;
+//
+//        for (i = 0; i < grades.size(); i++){
+//            if (grades.get(i) < average){
+//                belowAverage++;
+//            }
+//        }
+//
+//        System.out.println(belowAverage);
+
+    }
+
+
+
+}
---
+++ NoviceProblems/src/TipCalculatorProblem.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class TipCalculatorProblem {
+    public static void main(String[] args) {
+
+        // take an input for how much it costs
+        // take an input for a "terrible" "great"
+        // determine the total cost with tip
+        int[] arr = new int[3];
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextInt());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+//        double totalCost = 0;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Input the cost");
+//        double itemCost = sc.nextDouble();
+//        System.out.println("How was it? Either: terrible or great");
+//        String rating = sc.next();
+//
+//        if (rating.equals("terrible")) {
+//            totalCost = itemCost;
+//        }
+//        else if (rating.equals("great")) {
+//            totalCost = itemCost + itemCost * 0.2;      // woohoo! 20% tip!
+//        }
+//        System.out.println("Here's total cost: " + totalCost);
+
+    }
+
+}
